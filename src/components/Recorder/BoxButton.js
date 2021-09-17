07c8d9ba49496ebb0522aaeba019b479838468ac
takshch <takshch089@gmail.com>
2021-09-18 00:45:49 +0530

    Fixes send button color doesn't change

--- src/components/Recorder/BoxButton.js
+++ src/components/Recorder/BoxButton.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function BoxButton(props) {
   const { purple } = props;
-  const purpleClass = purple ? "box-button--purple" : "";
+  const purpleClass = purple === "true" ? "box-button--purple" : "";
   return (
     <button className={`box-button ${purpleClass}`} {...props}>
       {props.text}
